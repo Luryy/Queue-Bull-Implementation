37f08f68f1886ce8a6c960090f27982866f1a793
Lucas Yuri <lucasyuri56@hotmail.com>
2020-11-17 17:26:52 -0300

    feat: create bull-test

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
 import express from 'express';
 
+import Queue from './providers/QueueProvider/implementations/BullQueueProvider';
+
 const app = express();
+app.use(express.json());
+
+const queue = new Queue();
+
+queue.process();
+
+app.post('/bull-test', async (request, response) => {
+  const { teste } = request.body;
+
+  queue.add('TestJob', { teste });
+
+  response.json({ teste, data: new Date().getSeconds() });
+});
 
 export default app;
