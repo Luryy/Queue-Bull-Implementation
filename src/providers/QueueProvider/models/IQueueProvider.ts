127587f1ffe26e7f541e808bd6c3017b919157d5
Lucas Yuri <lucasyuri56@hotmail.com>
2020-11-17 17:25:37 -0300

    feat: create queue interface

---
+++ src/providers/QueueProvider/models/IQueueProvider.ts
@@ -0,0 +1,14 @@
+import { Job, Queue, JobOptions, DoneCallback } from 'bull';
+
+interface IJob {
+  bull: Queue;
+  name: string;
+  handle({ data }: { [key: string]: any }, done?: DoneCallback): Promise<void>;
+  options: JobOptions;
+}
+
+export default interface IQueueProvider {
+  queues: IJob[];
+  add(name: string, data: { [key: string]: any }): Promise<Job | null>;
+  process(): Promise<void | null>;
+}
